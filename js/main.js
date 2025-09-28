4bacc11c4274ed0e44040e27e06b265f09e5e2cd
m1hkh01 <ng.minhkhoi2912@gmail.com>
2025-09-28 22:14:24 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Load header & footer
+fetch("header.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("header").innerHTML = data);
+
+fetch("footer.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("footer").innerHTML = data);
